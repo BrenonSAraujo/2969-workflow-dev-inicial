e56ca894673e1d2605e2648ee71801627ab70448
BrenonSAraujo <brenonsilvestre64@gmail.com>
2024-12-06 03:52:45 -0300

    feat: :sparkles: add: flags de permissões com o unleash.

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,20 +1,21 @@
-import Evento from '../models/eventos.js'
+import Evento from '../models/eventos.js';
+import unleash from '../services/unleash.js';
 
 class EventosController {
-    static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+  static liberaAcessoEventos = () => unleash.isEnabled('eventos');
 
-    static listarEventos = async (req, res) => {
-        if (this.liberaAcessoEventos()) {
-            try {
-                const resultado = await Evento.pegarEventos();
-                return res.status(200).json(resultado)
-            } catch (err) {
-                return res.status(500).json(err.message)
-            }
-        } else {
-            return res.status(404).send();
-        }
+  static listarEventos = async (req, res) => {
+    if (this.liberaAcessoEventos()) {
+      try {
+        const resultado = await Evento.pegarEventos();
+        return res.status(200).json(resultado);
+      } catch (err) {
+        return res.status(500).json(err.message);
+      }
+    } else {
+      return res.status(404).send();
     }
+  };
 }
 
 export default EventosController;
